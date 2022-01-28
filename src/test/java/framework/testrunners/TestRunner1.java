99cd72481c6d75108e506f8d0256013c094443e5
Arnab Dey Sarkar <a.d.sarkar1990@gmail.com>
2022-01-28 23:57:45 +0530

    Updated Code

---
+++ src/test/java/framework/testrunners/TestRunner1.java
@@ -0,0 +1,29 @@
+package framework.testrunners;
+
+import cucumber.api.CucumberOptions;
+import framework.stepdefinitions.AbstractSteps;
+import framework.utilities.AbstractCucumberTestNG;
+import org.testng.annotations.*;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {"framework.stepdefinitions"},
+        plugin ={"com.cucumber.listener.ExtentCucumberFormatter:BDD_REPORTS/BDD-REPORT.html"},
+        tags = {"@UI"},
+        dryRun = false,monochrome = true)
+
+public class TestRunner1 extends AbstractCucumberTestNG{
+
+    @BeforeMethod
+    public void driverStart() throws Exception {
+        AbstractSteps.startDriver();
+    }
+    @AfterMethod
+    public void driverTeardown()  {
+        AbstractSteps.stopDriver();
+    }
+    @AfterClass
+    void terminate () {
+        AbstractSteps.terminateDriver();
+    }
+}
